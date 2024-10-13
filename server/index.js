1ca5c61a25a3fbf630bc81bb2b97dbb7fbb8afd7
nup chan <bao96065@gmail.com>
2024-10-14 00:40:25 +0700

    chore: change server and app port

--- server/index.js
+++ server/index.js
@@ -12,24 +12,3 @@ connectDB()
 app.listen(3000, () => {
     console.log('app is currently running')
 })
-
-app.get('/tshirt', (req, res) => {
-
-    res.status(200).send({
-        thepinkcat :'hello'
-    })
-
-})
-
-app.post('/tshirt/:id', (req,res) => {
-    const {id} = req.params;
-    const {logo} = req.logo;
-
-    if (!logo) {
-        res.status(418).send({Message: 'we need a logo'})
-    }
-
-    res.send({
-        tshirt: `cai ao nay with your ${logo} and id of ${id}`
-    })
-})
--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: 'http://localhost:5000',
+                url: 'http://localhost:5300',
             }
         ]
     },
@@ -62,7 +62,7 @@ app.use('/api/prizes', prizeRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Khởi động server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
