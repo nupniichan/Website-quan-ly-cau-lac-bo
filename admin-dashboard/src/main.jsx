0fee81112c7fcec213868e7f61c45c33bf3f80b4
Tequinzyy <tonyminhquan450@gmail.com>
2024-10-16 23:13:38 +0700

    new admin dashboard

---
+++ admin-dashboard/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
+import { MaterialTailwindControllerProvider } from "@/context";
+import App from "./App";
+import "./tailwind.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <MaterialTailwindControllerProvider>
+          <App />
+        </MaterialTailwindControllerProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
