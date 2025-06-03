60f79a69f6ac5d0e41016700051f00f15ee2b064
Minh Quân <116754124+Tequinzyy@users.noreply.github.com>
2025-06-03 11:09:08 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,10 +41,7 @@ const storage = multer.diskStorage({
 // Kết nối MongoDB
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     }
     catch (error){
